c6148cbd5c3fcf932d613a15de8a9b9d563e813d
Rajeev <s2100155@edu.bc.fi>
2021-03-29 12:14:55 +0300

    state personinside class

--- boxes/src/Main.js
+++ boxes/src/Main.js
@@ -2,13 +2,47 @@ import React, { Component } from "react";
 import Box from "./Box";
 
 class Main extends Component {
+  state = {
+    persons: [
+      {
+        name: "Jon",
+        age: 24,
+        title: "CEO",
+      },
+      {
+        name: "Cat",
+        age: 24,
+        title: "Technican",
+      },
+      {
+        name: "Dom",
+        age: 24,
+        title: "Developer",
+      },
+    ],
+  };
+
   render() {
     return (
       <main>
         <div>
-          <Box name="Raj Sah" age="25" title="Teacher" />
-          <Box name="John Doe" age="35" title="Doctor" />
-          <Box name="Mick jimi" age="45" title="Developer" />
+          <Box
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+            title={this.state.persons[0].title}
+          />
+
+          <Box
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            title={this.state.persons[1].title}
+          />
+
+          <Box
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+            title={this.state.persons[2].title}
+          />
         </div>
       </main>
     );
